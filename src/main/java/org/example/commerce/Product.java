b8592d408ea2f5556d3d61e7fd6a3c0274a62cf7
sosugyeong <sugyeong247@gmail.com>
2026-01-22 19:55:24 +0900

    상품 수정 기능에 쓸 setter 생성

--- src/main/java/org/example/commerce/Product.java
+++ src/main/java/org/example/commerce/Product.java
@@ -6,20 +6,29 @@ public class Product {
     private int stock;
     private String comment;
 
+    //getter
     public String getProductName() {
         return productName;
     }
-
     public int getPrice() {
         return price;
     }
-
     public int getStock() {
         return stock;
     }
-
     public String getComment() {
         return comment;
+    }
+
+    //setter
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    public void setComment(String comment) {
+        this.comment = comment;
     }
 
     public Product(String productName, int price, int stock, String comment){
