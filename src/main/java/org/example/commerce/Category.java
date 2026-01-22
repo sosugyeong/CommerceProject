64a0baeb693e1e72a4720d90e5b15bf11d6a7527
sosugyeong <sugyeong247@gmail.com>
2026-01-22 19:58:08 +0900

    상품 검색 메서드, 상품 삭제 메서드, 새로운 상품 정보 기존 카테고리에 추가하는 메서드 생성

--- src/main/java/org/example/commerce/Category.java
+++ src/main/java/org/example/commerce/Category.java
@@ -19,4 +19,32 @@ public class Category {
     public String getCategoryName(){ //카테고리 이름을 반환하는 메서드
         return categoryName;
     }
+
+    //새로운 상품 정보를 입력받아 카테고리에 추가
+    public void addProduct(String productNm, int price, int stock, String comment){
+        products.add(new Product(productNm, price, stock, comment));
+    }
+
+    //상품 검색 (상품 수정/삭제)
+    public Product findProduct(String productNm){
+        for (Product p : products){
+            if (p.getProductName().equals(productNm)){
+                return p; //찾은 상품을 통째로 반환
+            }
+        }
+        return null; //못찾으면 null 반환
+    }
+
+    //검색해서 나온 상품을 삭제
+    public void delete(String productNm){
+        Product p = findProduct(productNm);
+
+        if (p != null){
+            System.out.printf("%s 상품을 삭제합니다.%n", p.getProductName());
+            products.remove(p);
+            System.out.println("삭제가 완료되었습니다.");
+        } else {
+            System.out.println("삭제할 상품을 찾을 수 없습니다.");
+        }
+    }
 }
